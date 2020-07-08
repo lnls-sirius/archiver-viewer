46a597de592567ac8396fdbc312de97d828aaf47
Guilherme Francisco de Freitas <guifreitas89@hotmail.com>
2020-07-08 12:15:17 -0300

    Small performance improvements

--- src/index.js
+++ src/index.js
@@ -71,13 +71,19 @@ $(document).ready(function () {
                         unit: 'minute',
                         unitStepSize: 5,
                         displayFormats: {
-                            minute: 'DD/MM/YYYY HH:mm:ss'
+                           	second: 'hh:mm:ss',
+	    			minute: 'hh:mm',
+				hour: 'hh:ss',
+				day: 'MMM D hh:mm',
+				month: 'MMM YYYY'
                         },
                         tooltipFormat: 'ddd MMM DD YYYY HH:mm:ss.SSS ZZ',
                     },
                     ticks: {
-                        autoSkip : true,
+                        autoSkip: true,
                         autoSkipPadding: 5,
+			maxRotation: 0,
+			minRotation: 0
                     }
                 }],
                 yAxes: [{
