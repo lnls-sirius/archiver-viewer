f7435fbe18efdeec55df468b87942fd8c5ac871e
Guilherme F. de Freitas <guifreitas89@hotmail.com>
2020-09-18 10:29:06 -0300

    Decreases cached date hold to a lower value

--- src/lib/control.js
+++ src/lib/control.js
@@ -543,7 +543,7 @@ module.exports = (function () {
     async function getDateNow() {
     //console.log(archInterface.bypassUrl());
     if(!serverDate_enabled){return new Date();}
-    if(new Date() - lastFetch < 10000){return cachedDate;}
+    if(new Date() - lastFetch < 2000){return cachedDate;}
     try {
         const result = await $.ajax ({
             url: "http://" + archInterface.bypassUrl() +"/date",
