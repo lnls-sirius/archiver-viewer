7ca0f606312c2b01165b2b2d4e68d6b71ea31567
carneirofc <claudiofcarneiro@hotmail.com>
2020-11-20 17:13:40 -0300

    Include Series component

--- src/components/Series/index.jsx
+++ src/components/Series/index.jsx
@@ -1,43 +1,81 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import Checkbox from "../Checkbox";
 
-class Series extends Component {
-  constructor(props) {
-    super();
-
-    this.state = {
-      pvName: "PVName",
-      color: "#ff0000",
-      optimize: false,
-      logScale: false,
-    };
-  }
-  flip = (e) => {
-    this.setState({ optimize: e.target.checked });
-  };
-  color = (e) => {
-    this.setState({ color: e.target.value });
-  };
-
-  render() {
-    return (
-      <span>
-        <span className="color-picker">
-          <input type="color" value={this.state.color} onChange={this.color} />
-        </span>
-        <span>{this.state.pvName}</span>
-        <button>Info</button>
-        <label>
-          <input type="checkbox" onChange={this.flip} checked={this.props.optimize} />
-          <span className="checkmark"></span>
-          <label>Optimize?</label>
-        </label>
-        <button>X</button>
-      </span>
-    );
-  }
-}
-Series.propTypes = {
-  optimize: PropTypes.bool.isRequired,
+import { toggleAxisType, toggleAutoY } from "../../lib/control";
+import { setAxisYLimitManual, setAxisYLimitMax, setAxisYLimitMin } from "../../features/chart/sliceChart";
+const mapStateToProps = (state) => {
+  return { dataAxis: state.chart.dataAxis };
 };
-export default Series;
+const mapDispatch = { setAxisYLimitManual, setAxisYLimitMax, setAxisYLimitMin };
+import * as S from "./styled";
+const KEY_ENTER = 13;
+const Series = ({ dataAxis, setAxisYLimitManual, setAxisYLimitMax, setAxisYLimitMin }) => {
+  const [yMinState, setYMin] = useState("");
+  const [yMaxState, setYMax] = useState("");
+
+  const handleYMin = (e, id) => {
+    setYMin(e.target.value);
+  };
+
+  const handleYMax = (e, id) => {
+    setYMax(e.target.value);
+  };
+
+  const handleSubmitYMin = (e, id) => {
+    if (e.which !== KEY_ENTER) return;
+
+    const val = parseFloat(yMinState);
+    if (val) {
+      setAxisYLimitMin({ id: id, yMin: val });
+    }
+  };
+
+  const handleSubmitYMax = (e, id) => {
+    if (e.which !== KEY_ENTER) return;
+
+    const val = parseFloat(yMaxState);
+    if (val) {
+      setAxisYLimitMax({ id: id, yMax: val });
+    }
+  };
+
+  return (
+    <S.SeriesWrapper>
+      {dataAxis.map(({ id, type, display, yMin, yMax, yLimitManual, isLog }) => {
+        return (
+          <S.SerieWrapper key={id}>
+            Chart Series: <S.SerieName>{id}</S.SerieName>
+            <Checkbox text="Log" tooltip="Logarithmic axis" checked={isLog} onClick={() => toggleAxisType(id)} />
+            <Checkbox
+              text="Manual Y Limit"
+              tooltip="Manually define Y limits"
+              checked={yLimitManual}
+              onClick={() => setAxisYLimitManual({ id: id, yLimitManual: !yLimitManual })}
+            />
+            <S.InputWarpper>
+              <S.Input
+                $visible={yLimitManual}
+                onChange={(e) => handleYMax(e, id)} /*onKeyDown={handleSubmit}*/
+                onKeyDown={(e) => handleSubmitYMax(e, id)}
+                placeholder={`yMax ${yMax ? yMax : ""}`}
+                type="text"
+                value={yMaxState}
+              />
+              <S.Input
+                $visible={yLimitManual}
+                onChange={(e) => handleYMin(e, id)} /*onKeyDown={handleSubmit}*/
+                onKeyDown={(e) => handleSubmitYMin(e, id)}
+                placeholder={`yMin ${yMin ? yMin : ""}`}
+                type="text"
+                value={yMinState}
+              />
+            </S.InputWarpper>
+          </S.SerieWrapper>
+        );
+      })}
+    </S.SeriesWrapper>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatch)(Series);
