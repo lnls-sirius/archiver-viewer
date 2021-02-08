3edd7a2c45e8a58fb90e1f011046ccaf03739070
carneirofc <claudiofcarneiro@hotmail.com>
2021-02-08 17:55:18 -0300

    Include data export warning

--- src/use-cases/ExportDataset/impl.ts
+++ src/use-cases/ExportDataset/impl.ts
@@ -1,6 +1,7 @@
 import ExportDataset from "./interface";
 import DataExportInterface from "../../controllers/DataExport/DataExportInterface";
 import control from "../../entities/Chart/Chart";
+import { StatusDispatcher } from "../../utility/Dispatchers";
 
 class ExportDatasetImpl implements ExportDataset {
   private dataExportStrategy: DataExportInterface;
@@ -11,7 +12,9 @@ class ExportDatasetImpl implements ExportDataset {
 
   async asXlsx(): Promise<void> {
     if (control.isAutoUpdateEnabled()) {
-      console.error("Cannot export data while auto is enabled");
+      const msg = "Cannot export data while auto is enabled";
+      console.error(msg);
+      StatusDispatcher.Warning("Data export", msg);
       return;
     }
     const datasets = control.chart().data.datasets;
