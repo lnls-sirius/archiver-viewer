af85d8c7f6cc6b64a0220431504bdc2be96340fe
carneirofc <claudiofcarneiro@hotmail.com>
2021-02-10 19:46:38 -0300

    Include Search dispatcher

--- src/utility/Dispatchers/index.ts
+++ src/utility/Dispatchers/index.ts
@@ -1,4 +1,7 @@
 import RequestsDispatcher from "./Requests";
 import StatusDispatcher from "./Status";
+import SearchDispatcher from "./Search";
 
-export { RequestsDispatcher, StatusDispatcher };
+// @todo: This module could be used to instantiate and inject dependencies
+
+export { RequestsDispatcher, StatusDispatcher, SearchDispatcher };
