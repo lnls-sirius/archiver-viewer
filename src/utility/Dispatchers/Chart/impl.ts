d4ccadad33845d382b0f9ffb76456db1b0c90476
carneirofc <claudiofcarneiro@hotmail.com>
2021-03-19 15:06:13 -0300

    Include ChartDispatcher

--- src/utility/Dispatchers/Chart/impl.ts
+++ src/utility/Dispatchers/Chart/impl.ts
@@ -3,6 +3,24 @@ import store from "../../../store";
 import { actions } from "../../../features/chart";
 
 class ChartDispatcherImpl implements ChartDispatcher {
+  setWindowTime(windowTime: number): void {
+    store.dispatch(actions.setWindowTime(windowTime));
+  }
+  setTimeReferenceEnd(timeReferenceEnd: boolean): void {
+    store.dispatch(actions.setTimeReferenceEnd(timeReferenceEnd));
+  }
+  setSingleTooltipEnabled(enabled: boolean): void {
+    store.dispatch(actions.setSingleTooltip(enabled));
+  }
+  setZooming(zooming: boolean): void {
+    store.dispatch(actions.setZooming(zooming));
+  }
+  setDatasetOptimized(index: number, optimized: boolean): void {
+    store.dispatch(actions.setDatasetOptimized({ index, optimized }));
+  }
+  doRemoveDataset(index: number, removeAxis: string): void {
+    store.dispatch(actions.removeDataset({ idx: index, removeAxis }));
+  }
   setAxisYLimitManual(axisId: string, manual: boolean): void {
     store.dispatch(actions.setAxisYLimitManual({ id: axisId, yLimitManual: manual, yMin: "", yMax: "" }));
   }
