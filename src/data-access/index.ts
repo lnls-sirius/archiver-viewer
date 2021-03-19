2b25461876da3bbe41297d4087d5899351d5275a
carneirofc <claudiofcarneiro@hotmail.com>
2021-03-19 15:07:18 -0300

    Rename data-access/

--- src/data-access/index.ts
+++ src/data-access/index.ts
@@ -1,5 +1,5 @@
 import { DataAccess } from "./interface";
-import ArchiverDataAccessFactory from "./ArchiverDataAccessFactory";
+import ArchiverDataAccessFactory from "./factory";
 
 const archInterface: DataAccess = ArchiverDataAccessFactory();
 export default archInterface;
